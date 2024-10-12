ddeebc68e70193a566be6e696936d3bef342d9b5
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-10-12 09:06:02 -0500

    Added Packed/Fused Amber

--- amber.lua
+++ amber.lua
@@ -13,15 +13,101 @@ local amberbug	= "(" ..coalstone.. ")^[mask:nc_woodwork_ladder_mask.png"
 minetest.register_craftitem(modname .. ":amber", {
 	description = "Amber",
 	inventory_image = amber,
-	groups = {amber = 1, fossil = 1},
+	groups = {amber = 1, fossil = 1, lux_absorb = 8},
 	sounds = nodecore.sounds("nc_optics_glassy")
 })
 minetest.register_craftitem(modname .. ":amber_bug", {
 	description = "Amber",
 	inventory_image = amber.. "^(" ..amberbug.. "^[opacity:100)",
-	groups = {amber = 1, fossil = 1, bugamber = 1},
+	groups = {amber = 1, fossil = 1, bugamber = 1, lux_absorb = 8},
 	sounds = nodecore.sounds("nc_optics_glassy")
 })
+-----<>----------------------------------------------------------<>-----
+minetest.register_node(modname .. ":amber_fused", {
+	description = "Fused Amber",	
+	drawtype = "glasslike_framed",
+	tiles = {
+		modname .. "_fused.png^" .. modname .. "_amber_edges.png",
+		modname.. "_fused.png"
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	groups = {
+		cracky = 1,
+		lux_absorb = 32,
+		fossil = 1,
+		amber_node = 1,
+		scaling_time = 240
+	},
+	sounds = nodecore.sounds("nc_optics_glassy"),
+	visinv_bulk_optimize = true,
+	no_repack = true,
+	alternate_loose = {
+		description = "Packed Amber",
+		tiles = {modname.. "_fused.png^nc_api_loose.png"},
+		use_texture_alpha = "clip",
+		groups = {
+			cracky = 0,
+			crumbly = 2,
+			lux_absorb = 24,
+			scaling_time = 120,
+			falling_repose = 1,
+			amber_node = 1,
+		},
+		sounds = nodecore.sounds("nc_terrain_crunchy"),
+	}
+})
+-----<>----------------------------------------------------------<>-----
+nodecore.register_craft({
+	label = "pack amber",
+	action = "pummel",
+	toolgroups = {thumpy = 3},
+	indexkeys = {modname.. ":amber"},
+	nodes = {
+		{
+			match = {name = modname .. ":amber", count = 8},
+			replace = modname.. ":amber_fused_loose"
+		}
+	}
+})
+local function brkambr(suff, cracky, umph)
+	return nodecore.register_craft({
+		label = "break amber nodes apart",
+		action = "pummel",
+		toolgroups = {cracky = cracky},
+		indexkeys = {modname .. ":amber_fused" .. suff},
+		nodes = {
+			{
+				match = {name = modname .. ":amber_fused" .. suff},
+				replace = "air"
+			}
+		},
+		items = {
+			{name = modname.. ":amber", count = 8, scatter = umph}
+		},
+		itemscatter = umph,
+	})
+end
+brkambr("_loose", 2, 3)
+brkambr("", 3, 6)
+-----<>----------------------------------------------------------<>-----
+nodecore.register_craft({
+	label = "fuse amber",
+	action = "cook",
+	touchgroups = {flame = 3},
+	neargroups = {coolant = 0},
+	duration = 30,
+	cookfx = true,
+	indexkeys = {modname.. ":amber_fused_loose"},
+	nodes = {
+		{
+			match = {name = modname.. ":amber_fused_loose"},
+			replace = modname .. ":amber_fused"
+		}
+	}
+})
+--nodecore.register_cook_abm({nodenames = {modname.. ":amber_fused_loose"}, neighbors = {"group:flame"}})
+--nodecore.register_cook_abm({nodenames = {modname .. ":amber_fused"}})
 -----<>----------------------------------------------------------<>-----
 local adzedef
 adzedef = {
@@ -29,14 +115,15 @@ adzedef = {
 	inventory_image = "nc_woodwork_adze.png^(nc_stonework_tip_adze.png^[colorize:gold:120)",
 	groups = {
 		firestick = 2,
-		flammable = 2
+		flammable = 2,
+		lux_absorb = 8
 	},
 	tool_wears_to = "nc_woodwork:adze",
 	tool_capabilities = nodecore.toolcaps({
 			uses = 1.25,
 			choppy = 2,
 			crumbly = 3,
-			cracky = 1
+			cracky = 1,
 		}),
 	sounds = nodecore.sounds("nc_optics_glassy"),
 }
@@ -71,7 +158,8 @@ local function tooltip(name, group)
 			inventory_image = "nc_woodwork_tool_" .. name:lower() .. ".png^(nc_stonework_tip_" .. name:lower() .. ".png^[colorize:gold:120)",
 			tool_wears_to = wood,
 			groups = {
-				flammable = 2
+				flammable = 2,
+				lux_absorb = 8
 			},
 			tool_capabilities = nodecore.toolcaps({
 					uses = 1.25,
